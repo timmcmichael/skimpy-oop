7b72706990d793574e9d775f9a0052473c37c493
Tim <tim.mcmichael@estrellamountain.edu>
2025-01-04 11:54:50 -0700

    edits to code samples

--- chapter-2-variables/source/InputDemoFinished.java
+++ chapter-2-variables/source/InputDemoFinished.java
@@ -4,6 +4,29 @@ public class InputDemoFinished {
 
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
+        String name;
+        int age;
+        String email;
+
+        System.out.println("Enter your name: ");
+        name = input.nextLine();
+        System.out.print("Enter your age: ");
+        age = input.nextInt();
+        input.nextLine();
+        // age = age + 10;
+        age += 10;
+        System.out.print("Enter your age again: ");
+        age = input.nextInt();
+        input.nextLine();
+        // age = age + 10;
+        age += 10;
+
+        System.out.print("Enter your email address: ");
+        email = input.nextLine();
+
+        System.out.println("Hello, " + name);
+        System.out.println("In 10 years, you will be " + age + " years old.");
+        System.out.println("Email address = " + email);
 
     }
 
