161e5443bdae746be8ec7d1cf0b87ce35b917e12
Tim <tim.mcmichael@estrellamountain.edu>
2025-01-10 22:42:03 -0700

    ch 1 final prep and html updates

---
+++ chapter-1-coding/source/OutputExample.java
@@ -0,0 +1,9 @@
+public class OutputExample {
+
+    public static void main(String[] args) {
+        System.out.print("As the Rolling Stones might say,");
+        System.out.println("you can't always");
+        System.out.println("get what you want.");
+    }
+
+}
