b1d24b0818123dd66bdcfbfdea1a0754761ed5ea
Tim <tim.mcmichael@estrellamountain.edu>
2025-01-12 19:19:57 -0700

    todo updates

---
+++ chapter-2-variables/source/OutputtingVariables.java
@@ -0,0 +1,15 @@
+public class OutputtingVariables {
+
+    public static void main(String[] args) {
+        String artistName = "Sam Cooke";
+        int birthYear = 1931;
+
+        System.out.println(artistName + " was born in " + birthYear);
+
+        // System.out.print(artistName);
+        // System.out.print(" was born in ");
+        // System.out.println(birthYear);
+
+    }
+
+}
