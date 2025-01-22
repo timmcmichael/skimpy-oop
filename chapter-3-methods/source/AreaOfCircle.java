993c418f61a058832d440ed32614cf1235e05342
Tim McMichael <tim.mcmichael@estrellamountain.edu>
2025-01-22 08:15:13 -0700

    ch 3 work

---
+++ chapter-3-methods/source/AreaOfCircle.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ * Demo file
+ * 
+ * The demonstrates passing a arguments to a method by implementing a method
+ * that accepts a radius of a circle and outputs the area.
+ * 
+ * Test Cases:
+ * 1.0 -> 3.14
+ * 4.0 -> 50.27
+ * 15.67 -> 771.41462
+ * 
+ */
+public class AreaOfCircle {
+
+    public static void main(String[] args) {
+
+    }
+}
