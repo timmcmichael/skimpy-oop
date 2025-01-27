0f9ac2775d5ab9285b312839f8a54daf3b2bcedd
Tim McMichael <tim.mcmichael@estrellamountain.edu>
2025-01-26 22:30:42 -0700

    Ch 4 text

--- chapter-4-classes/source/WeatherRecord.java
+++ chapter-4-classes/source/WeatherRecord.java
@@ -4,28 +4,31 @@
  * 
  */
 public class WeatherRecord {
+    // Fields
     private String date;
     private int highTemperature;
     private double averageWindSpeed;
     
+    // Getters
     public String getDate() {
         return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
     }
 
     public int getHighTemperature() {
         return highTemperature;
     }
 
-    public void setHighTemperature(int highTemperature) {
-        this.highTemperature = highTemperature;
-    }
-
     public double getAverageWindSpeed() {
         return averageWindSpeed;
+    }
+
+    // Setters and Mutators
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setHighTemperature(int highTemperature) {
+        this.highTemperature = highTemperature;
     }
 
     public void setAverageWindSpeed(double averageWindSpeed) {
