525bca2897d3db757d69ffb62680e1edb4270341
Tim McMichael <tim.mcmichael@estrellamountain.edu>
2025-01-31 08:26:36 -0700

    video code update

--- chapter-4-classes/source/CellCustomer.java
+++ chapter-4-classes/source/CellCustomer.java
@@ -1,47 +1,58 @@
 /*
+ * CellCustomer
  * This class represents a customer of a cell phone service company, focusing on the customer's data plan and usage.
  */
+
 public class CellCustomer {
 
-    // Fields (Instance Variables)
+    // Fields (instance variables)
     private String name;
     private String phoneNumber;
     private double monthlyData;
     private double remainingData;
 
-    // Getters (accessors)
+    // Getters
     public String getName() {
-        return name;
-    }
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-    public double getMonthlyData() {
-        return monthlyData;
-    }
-    public double getRemainingData() {
-        return remainingData;
+        return this.name;
     }
 
-    // Setters (mutators)
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public double getMonthlyData() {
+        return this.monthlyData;
+    }
+
+    public double getRemainingData() {
+        return this.remainingData;
+    }
+
+    // Setters
     public void setName(String cName) {
         this.name = cName;
     }
+
     public void setPhoneNumber(String cPhoneNumber) {
         this.phoneNumber = cPhoneNumber;
     }
+
     public void setMonthlyData(double cMonthlyData) {
         this.monthlyData = cMonthlyData;
         this.remainingData = cMonthlyData;
     }
 
+    // Mutators
     public void useData(double dataUsed) {
         this.remainingData -= dataUsed;
     }
 
-    public void topUpData() {
-        this.remainingData = this.monthlyData;
+    public void addData(double amount) {
+        this.remainingData += amount;
     }
 
+    public void resetData() {
+        this.remainingData = this.monthlyData;
+    }
     
 }
