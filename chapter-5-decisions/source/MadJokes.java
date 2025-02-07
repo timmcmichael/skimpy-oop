6a8784229c61e1577e7a43246374f152dd392607
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-02-06 20:13:31 -0700

    Video code
    Replaced file with code actually written during the recording of the video

--- chapter-5-decisions/source/MadJokes.java
+++ chapter-5-decisions/source/MadJokes.java
@@ -1,14 +1,17 @@
 /*
  * MadJokes
- * This program prompts the user to input how many jokes they'd like to buy at $30/each. If the user buys 10 or more, they'll get a 10% discount. The program will then display the total cost of the jokes.
+ * This program prompts the user to input how many jokes they'd like to buy at $30/joke. If the user buys 10 or more, they'll get a 10% discount. The program will then display the total cost of the jokes.
  * 
  * Test Cases:
- * 1 -> $30.0
- * 9 -> $270.0
- * 10 -> $270.0
- * 11 -> $297.0
+ * 1 -> 30.0
+ * 9 -> 270.0
+ * 10 -> 270.0
+ * 11 -> 297.0
  * 
  */
+
+package module04;
+
 
 import java.util.Scanner;
 
@@ -21,18 +24,17 @@ public class MadJokes {
 
         System.out.print("How many jokes would you like to buy? ");
         numJokes = input.nextInt();
-        input.close();
 
-        double totalCost = numJokes * 30;
+        totalPrice = numJokes * 30.0;
 
         if (numJokes >= 10) {
-            totalCost = totalCost * .9;
+            totalPrice = totalPrice * .9; // totalPrice *= .9 - same thing!
         } else {
-            System.out.println(
-                    "Did you know that if you buy 10 or more jokes, you get a 10% discount? Keep that in mind for your next purchase!");
+            System.out.println("Did you know that if you buy 10 or more jokes, you get a 10% discount? Keep that in mind for your next purchase!");
         }
-        System.out.println("The total cost is $" + totalCost);
 
+
+        System.out.println("The total cost of you joke purchase is $" + totalPrice);
     }
 
 }
