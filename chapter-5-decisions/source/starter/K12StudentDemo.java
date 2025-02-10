f63725163675222f3707f0f3adb8aecde915f59d
Tim McMichael <tim.mcmichael@estrellamountain.edu>
2025-02-10 11:11:47 -0700

    recording prep

--- chapter-5-decisions/source/starter/K12StudentDemo.java
+++ chapter-5-decisions/source/starter/K12StudentDemo.java
@@ -10,28 +10,28 @@ public class K12StudentDemo {
                 System.out.println(student1.getName() + " is a(n) " + student1.getSchoolType() + " student in grade "
                                 + student1.getGradeLevel());
 
-                K12Student student2 = new K12Student();
-                student2.setName("Carlos");
-                student2.setAge(10);
-                System.out.println(student2.getName() + " is a(n) " + student2.getSchoolType() + " student in grade "
-                                + student2.getGradeLevel());
+                // K12Student student2 = new K12Student();
+                // student2.setName("Carlos");
+                // student2.setAge(10);
+                // System.out.println(student2.getName() + " is a(n) " + student2.getSchoolType() + " student in grade "
+                //                 + student2.getGradeLevel());
 
-                K12Student student3 = new K12Student();
-                student3.setName("Ling");
-                student3.setAge(15);
-                System.out.println(student3.getName() + " is a(n) " + student3.getSchoolType() + " student in grade "
-                                + student3.getGradeLevel());
+                // K12Student student3 = new K12Student();
+                // student3.setName("Ling");
+                // student3.setAge(15);
+                // System.out.println(student3.getName() + " is a(n) " + student3.getSchoolType() + " student in grade "
+                //                 + student3.getGradeLevel());
 
-                K12Student student4 = new K12Student();
-                student4.setName("Jeff");
-                student4.setAge(122);
-                System.out.println(student4.getName() + " is a(n) " + student4.getSchoolType() + " student in grade "
-                                + student4.getGradeLevel());
+                // K12Student student4 = new K12Student();
+                // student4.setName("Jeff");
+                // student4.setAge(122);
+                // System.out.println(student4.getName() + " is a(n) " + student4.getSchoolType() + " student in grade "
+                //                 + student4.getGradeLevel());
 
-                System.out.println("Correcting Jeff's age...");
-                student4.setAge(12);
-                System.out.println(student4.getName() + " is a(n) " + student4.getSchoolType() + " student in grade "
-                                + student4.getGradeLevel());
+                // System.out.println("Correcting Jeff's age...");
+                // student4.setAge(12);
+                // System.out.println(student4.getName() + " is a(n) " + student4.getSchoolType() + " student in grade "
+                //                 + student4.getGradeLevel());
         }
 
 }
