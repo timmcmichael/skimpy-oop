01884970a8a767b981a3387fa8cade593f445aac
Tim McMichael <tim.mcmichael@estrellamountain.edu>
2025-02-10 14:27:35 -0700

    After video recording

--- chapter-5-decisions/source/K12Student.java
+++ chapter-5-decisions/source/K12Student.java
@@ -4,19 +4,12 @@
  * It demonstrates the use of if-else statements in an object-oriented context, including encapsulation and data validation.
  * 
  */
-package module05;
-
 public class K12Student {
     private String name;
     private int age;
     private int gradeLevel;
 
     // Constructors
-
-    public K12Student(String name, int age) {
-        this.name = name;
-        this.setAge(age);
-    }
 
     // Getters
     public String getName() {
@@ -32,15 +25,7 @@ public class K12Student {
     }
 
     public String getSchoolType() {
-        if (this.gradeLevel == 0) {
-            return "kindergarten";
-        } else if (this.gradeLevel < 6) {
-            return "elementary school";
-        } else if (this.gradeLevel < 9) {
-            return "middle school";
-        } else {
-            return "high school";
-        }
+        return "[unknown]";
     }
 
     // Setters
@@ -50,13 +35,6 @@ public class K12Student {
 
     public void setAge(int age) {
         this.age = age;
-        if (this.age < 6) {
-            this.age = 6;
-        }
-        if (this.age > 18) {
-            this.age = 18;
-        }
-        this.gradeLevel = this.age - 6;
     }
 
 }
