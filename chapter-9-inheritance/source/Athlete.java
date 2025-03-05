b230332a89a8437fa9d2163dbec15016e7fe9d3a
Tim McMichael <tim.mcmichael@estrellamountain.edu>
2025-03-04 19:52:36 -0700

    Inheritance

---
+++ chapter-9-inheritance/source/Athlete.java
@@ -0,0 +1,17 @@
+public class Athlete {
+    private String athleteName;
+    private int age;
+    private int weight;
+
+    public void run() {
+        System.out.println("Running!");
+    }
+
+    public void jump() {
+        System.out.println("Jumping!");
+    }
+
+    public void throwBall() {
+        System.out.println("Throwing!");
+    }
+}
---
+++ chapter-9-inheritance/source/FootballPlayer.java
@@ -0,0 +1,12 @@
+public class FootballPlayer extends Athlete {
+    private String teamName;
+    private String position;
+
+    public void tackle() {
+        System.out.println("Tackling!");
+    }
+
+    public void pass() {
+        System.out.println("Passing!");
+    }
+}
